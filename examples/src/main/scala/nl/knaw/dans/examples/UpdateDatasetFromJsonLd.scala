9bb4b743583945dbde95f94771a63b487e2a697f
Jan van Mansum <janvanmansum@gmail.com>
2021-10-08 16:23:33 +0200

    Set license example

--- examples/src/main/scala/nl/knaw/dans/examples/UpdateDatasetFromJsonLd.scala
+++ examples/src/main/scala/nl/knaw/dans/examples/UpdateDatasetFromJsonLd.scala
@@ -26,19 +26,7 @@ object UpdateDatasetFromJsonLd extends App with DebugEnhancedLogging with BaseAp
   private val result = for {
     response <- server.dataset(persistentId).updateMetadataFromJsonLd(
       """{
-        |  "http://purl.org/dc/terms/title": "Darwin's Finches UPDATED 2",
-        |  "http://purl.org/dc/terms/subject": "Medicine, Health and Life Sciences",
-        |  "http://purl.org/dc/terms/creator": {
-        |      "https://dataverse.org/schema/citation/author#Name": "Finch, Fiona  UPDATED",
-        |      "https://dataverse.org/schema/citation/author#Affiliation": "Birds Inc  UPDATED."
-        |  },
-        |  "https://dataverse.org/schema/citation/Contact": {
-        |    "https://dataverse.org/schema/citation/datasetContact#E-mail": "finch@mailinator.com",
-        |    "https://dataverse.org/schema/citation/datasetContact#Name": "Finch, Fiona"
-        |  },
-        |  "https://dataverse.org/schema/citation/Description": {
-        |    "https://dataverse.org/schema/citation/dsDescription#Text": "Darwin's finches (also known as the Galápagos finches) are a group of about fifteen species of passerine birds."
-        |  }
+        |"http://schema.org/license": "http://creativecommons.org/publicdomain/zero/1.0"
         |}
         |""".stripMargin, replace = true)
     _ = logger.info(s"Raw response message: ${ response.string }")
